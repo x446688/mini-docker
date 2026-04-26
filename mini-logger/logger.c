f70d31bd0885a316134060f20d495f0b55149f8f
burnlngwolf <brylev2006@mail.ru>
2026-04-26 22:58:33 +0300

    variadic support and enum prefix for logger

--- mini-logger/logger.c
+++ mini-logger/logger.c
@@ -5,7 +5,15 @@
 #include <time.h>
 #include <stdarg.h>
 
-int logdoc(const char* level, const char* file, const char* text, ...)
+const char *log_level_strings[] = {
+  "   DEBUG",
+  "    INFO",
+  " WARNING",
+  "   ERROR",
+  "CRITICAL",
+};
+
+int logdoc(int level, const char* file, const char* text, ...)
 {
     FILE *fp = fopen(file, "a");
     if (fp == NULL) {
@@ -29,7 +37,7 @@ int logdoc(const char* level, const char* file, const char* text, ...)
     va_start(args, text);
 
     // Prefix
-    fprintf(fp, "[%s] [%s] ", timeStr, level);
+    fprintf(fp, "[%s] [%s] ", timeStr, log_level_strings[level]);
     // Variadic print
     vfprintf(fp, text, args);
     fprintf(fp, "\n");
--- mini-logger/logger.h
+++ mini-logger/logger.h
@@ -4,10 +4,10 @@
 
 int logdoc(const char* level, char* file, char* text, ...);
 
-const char *log_level_strings[] = {
-  "   DEBUG",
-  "    INFO",
-  "    WARN",
-  "   ERROR",
-  "CRITICAL",
+enum {
+	LOG_LVL_DEBUG, // 0
+	LOG_LVL_INFO, // 1
+	LOG_LVL_WARNING, // 2
+	LOG_LVL_ERROR, // 3
+	LOG_LVL_CRITICAL, // 4
 };
