a8db033351ee6042e0c5c93128d81985762b47a2
burnlngwolf <brylev2006@mail.ru>
2026-04-26 22:47:19 +0300

    Created variadic support for logger

--- mini-logger/logger.c
+++ mini-logger/logger.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
-#include <time.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <string.h>
-#include "logger.h"
+#include <errno.h>
+#include <time.h>
+#include <stdarg.h>
 
-int logdoc(char* level, char* file, char* text)
+int logdoc(const char* level, const char* file, const char* text, ...)
 {
-  FILE *fp = fopen(file, "a");
-  if (fp == NULL) {
-    if (errno == ENOENT) {
-        printf("Error: Bad path or symbolic link.\n");
-    } else if (errno == EACCES) {
-        printf("Error: Permission denied.\n");
-        return -1;
-    } else {
-        printf("Error: %s\n", strerror(errno));
+    FILE *fp = fopen(file, "a");
+    if (fp == NULL) {
+        if (errno == ENOENT) {
+            printf("Error: Bad path or symbolic link.\n");
+        } else if (errno == EACCES) {
+            printf("Error: Permission denied.\n");
+        } else {
+            printf("Error: %s\n", strerror(errno));
+        }
         return -1;
     }
-  }
 
-  time_t now;
-  time(&now);                      
-  struct tm *local = localtime(&now); 
-  char timeStr[20];
-  strftime(timeStr, sizeof(timeStr), "%H:%M:%S", local);
+    time_t now;
+    time(&now);
+    struct tm *local = localtime(&now);
+    char timeStr[32];
+    strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", local);
 
-  fprintf(fp, "[%s] [%s] %s\n", timeStr, level, text);
-  fclose(fp);
+    va_list args;
+    va_start(args, text);
 
-  return 1;
+    // Prefix
+    fprintf(fp, "[%s] [%s] ", timeStr, level);
+    // Variadic print
+    vfprintf(fp, text, args);
+    fprintf(fp, "\n");
+
+    va_end(args);
+    fclose(fp);
+
+    return 1;
 }
--- mini-logger/logger.h
+++ mini-logger/logger.h
@@ -1,12 +1,13 @@
 #ifndef LOGGER_H
 #define  LOGGER_H
-int logdoc(char* level, char* file, char* text);
 #endif
-static const char* log_level_strings [] = {
-	"NONE", // 0
-	"CRIT", // 1
-	"WARN", // 2
-	"NOTI", // 3
-	" LOG", // 4
-	"DEBG" // 5
+
+int logdoc(const char* level, char* file, char* text, ...);
+
+const char *log_level_strings[] = {
+  "   DEBUG",
+  "    INFO",
+  "    WARN",
+  "   ERROR",
+  "CRITICAL",
 };
