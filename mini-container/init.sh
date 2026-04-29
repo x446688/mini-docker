21cbcd4f17369234008533a83ab363d5ac4ffce8
Untitled312 <untitled.312.mail@gmail.com>
2026-04-29 11:11:16 +0300

    updated init.sh

--- mini-container/init.sh
+++ mini-container/init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-LIBDIR=$(test -d /lib64/ && echo "lib64" || echo "lib" )
+LIBDIR=lib
+LIBDIR64=lib64
 CONTAINER_DIR=$1
 if !(ls ${CONTAINER_DIR}); then
-    mkdir -p ${CONTAINER_DIR}/usr/bin ${CONTAINER_DIR}/lib ${CONTAINER_DIR}/${LIBDIR} ${CONTAINER_DIR}/var/log/
+    mkdir -p ${CONTAINER_DIR}/usr/bin ${CONTAINER_DIR}/${LIBDIR} ${CONTAINER_DIR}/${LIBDIR64} ${CONTAINER_DIR}/var/log/
     cp /usr/bin/busybox ${CONTAINER_DIR}/usr/bin
     ldd /usr/bin/busybox | awk '/ => / { print $3 }' > tmp.txt
     FILE="tmp.txt"
     while read -r line; do
         cp "$line" ${CONTAINER_DIR}/${LIBDIR}/.
+        cp "$line" ${CONTAINER_DIR}/${LIBDIR64}/.
     done <$FILE
     rm tmp.txt
-    cp /${LIBDIR}/ld-linux-*.so.* ${CONTAINER_DIR}/${LIBDIR}
+    cp /${LIBDIR}/ld-linux-*.so.* ${CONTAINER_DIR}/${LIBDIR64}
 fi
