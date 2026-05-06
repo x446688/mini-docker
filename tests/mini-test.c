a679b4893e17f6b078ba56d4569430482e197f18
Untitled312 <untitled.312.mail@gmail.com>
2026-05-06 08:17:50 -0400

    added output in logfile

--- tests/mini-test.c
+++ tests/mini-test.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
+#include "logger.h"
+#define LOGFILE "/var/log/mini-docker.log"
 
 int main() {
+    int counter = 0;
     while (1) {
+        logdoc(LOG_LVL_INFO, LOGFILE, "Debug: %d", counter);
+        counter++;
         sleep(2);
     }
 }
