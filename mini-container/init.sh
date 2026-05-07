aa5b0ac134019700b868100137371d05cb372b94
x446688 <xzp2byf924@privaterelay.appleid.com>
2026-05-07 23:13:52 +0300

    Fix init.sh container directory creation

--- mini-container/init.sh
+++ mini-container/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONTAINER_DIR=$1
 TEST_FILE=$2
-test -d $CONTAINER_DIR || exit -1
+test -d $CONTAINER_DIR || mkdir $CONTAINER_DIR
 curl -o minrootfs.tar.xz https://mirror.rosalab.ru/rosa/rosa2021.1/iso/ROSA.FRESH.12/rootfs/rootfs-minimal-rosa2021.1_x86_64_2022-11-02.tar.xz
 tar -xvf minrootfs.tar.xz -C $CONTAINER_DIR
 test -f $TEST_FILE && mv $TEST_FILE $CONTAINER_DIR/usr/bin
